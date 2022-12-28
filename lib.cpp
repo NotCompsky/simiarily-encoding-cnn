27e188980b3548b06e621453e18bb379257c8223
NotCompsky <adammarkgray@gmail.com>
2022-12-28 23:47:05 +0000

    Performance:  Use SIMD

--- lib.cpp
+++ lib.cpp
@@ -5,16 +5,23 @@
 #include <x86intrin.h> // Required for SIMD instructions on x86 architectures
 
 
+constexpr size_t _m256_float_step_sz = sizeof(__m256) / sizeof(float);
+
+
 float l2_norm(float* const arr){
-	float res = 0.0;
-	for (unsigned i = 0;  i < 1024;  ++i)
-		res += arr[i] * arr[i];
-	return std::sqrt(res);
+	__m256 product = _mm256_set1_ps(0.0);
+	for (unsigned i = 0;  i < 1024/8;  ++i){
+		__m256 row = _mm256_loadu_ps(arr + 8*i);
+		product += _mm256_mul_ps(row, row);
+	}
+	return std::sqrt(product[0] + product[1] + product[2] + product[3]);
 }
 void normalize(float* const flat_arr){
-	const float norm = l2_norm(flat_arr);
-	for (std::size_t i = 0;  i < 1024;  ++i){
-		flat_arr[i] /= norm;
+	const __m256 norm = _mm256_set1_ps(l2_norm(flat_arr)); // NOTE: Requires "-march=native" otherwise you get the error: "inlining failed in call to always_inline '__m256 _mm256_set1_ps(float)': target specific option mismatch"
+	// this takes the float value from l2_norm, creates a __m256 (aka 8 floats) set to that value
+	for (std::size_t i = 0;  i < 1024/8;  ++i){
+		__m256 hwvec2 = _mm256_load_ps(&flat_arr[i * _m256_float_step_sz]);
+		_mm256_store_ps(&flat_arr[i * _m256_float_step_sz], _mm256_div_ps(hwvec2, norm));
 	}
 }
 
@@ -24,27 +31,44 @@ void cosine_similarity(float* const vecarr,  float* const result,  const std::si
 	}
 	for (int i = 0;  i < num_rows;  ++i){
 		for (int j = 0;  j < num_rows;  ++j){
-			result[num_rows*j + i] = 0;
-			for (int k = 0;  k < 1024;  ++k) {
-				result[num_rows*j + i] += vecarr[1024*i + k] * vecarr[1024*j + k];
+			__m256 product = _mm256_set1_ps(0.0);
+			for (int k = 0;  k < 1024/8;  ++k){
+				__m256 row1 = _mm256_load_ps(&vecarr[1024*i + 8*k]);
+				__m256 row2 = _mm256_load_ps(&vecarr[1024*j + 8*k]);
+				__m256 product2 = _mm256_mul_ps(row1, row2);
+				product = _mm256_add_ps(product, product2);
 			}
+			result[num_rows*j + i] = product[0] + product[1] + product[2] + product[3];
 		}
-		// speed is 200 per 5s
+		// speed is 200 per 1s
 		if (i%200 == 0)
 			printf("DONE %i\n", i);
 	}
-	printf("DONE cosine_similarity\n"); fflush(stdout);
+}
+
+
+void* aligned_malloc(const std::size_t required_bytes,  const std::size_t alignment){
+	void* orig_ptr;
+	void** p2; // aligned block
+	int offset = alignment - 1 + sizeof(void*);
+	if ((orig_ptr = (void*)malloc(required_bytes + offset)) == nullptr){
+		return nullptr;
+	}
+	p2 = (void**)(((size_t)(orig_ptr) + offset) & ~(alignment - 1));
+	p2[-1] = orig_ptr;
+	return p2;
 }
 
 extern "C"
 float* cosine_similarity_from_numpy_contiguous_array(float* const float_array_ptr,  const std::size_t n_elements){
-	alignas(16) float* const vecarr = reinterpret_cast<float*>(malloc(n_elements*1024*sizeof(float)));
+	float* const vecarr = reinterpret_cast<float*>(aligned_malloc(n_elements*1024*sizeof(float), 32));
 	for (size_t i = 0;  i < n_elements;  ++i){
 		memcpy(vecarr + 1024*i,  float_array_ptr + i * 1024,  1024*sizeof(float));
 	}
-	float* const result = reinterpret_cast<float*>(malloc(n_elements*n_elements*sizeof(float)));
+	float* const result = reinterpret_cast<float*>(aligned_malloc(n_elements*n_elements*sizeof(float), 32));
+	//memset(result, 0, n_elements*n_elements*sizeof(float));
 	cosine_similarity(vecarr, result, n_elements);
-	free(vecarr);
-	printf("ptr == %lu\n", (uint64_t)result); fflush(stdout);
+	void* vecarr_orig_ptr = &vecarr[-1];
+	free(vecarr_orig_ptr);
 	return result;
 }
